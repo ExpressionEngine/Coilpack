a9f18adfff4aea3cd9f033dd92af80d0e6e96a97
Stephen Galbraith <109251940+nerdgency@users.noreply.github.com>
2023-02-23 15:25:12 -0500

    Update CoilpackServiceProvider.php
    Adds a new CoilpackUserProvider callback to Laravel's Authentication Provider.

--- src/CoilpackServiceProvider.php
+++ src/CoilpackServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Expressionengine\Coilpack;
 
+use Expressionengine\Coilpack\Auth\CoilpackUserProvider;
 use Expressionengine\Coilpack\Api\Graph\Support\FieldtypeRegistrar;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Event;
@@ -73,6 +74,10 @@ class CoilpackServiceProvider extends ServiceProvider
             if (\Illuminate\Support\Str::startsWith($event->route->uri, config('graphql.route.prefix'))) {
                 app(FieldtypeRegistrar::class)->boot();
             }
+        });
+
+        \Illuminate\Support\Facades\Auth::provider('coilpack', function($app, array $config) {
+            return new CoilpackUserProvider($app->make($config['model']));
         });
     }
 
