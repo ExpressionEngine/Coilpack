6bc70f5bdbf3cc23d73bafa276b9e0353e89e76b
Stephen Galbraith <109251940+nerdgency@users.noreply.github.com>
2023-02-23 15:26:37 -0500

    Create CoilpackUserProvider.php
    Implements the CoilpackUserProvider
    
    - Respects the default EE Member Model or the model in config/coilpack.php
    - Uses EE's authentication functions to create the appropriate session

---
+++ src/Auth/CoilpackUserProvider.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Expressionengine\Coilpack\Auth;
+
+use Expressionengine\Coilpack\Models\Member\Member;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\UserProvider;
+
+class CoilpackUserProvider implements UserProvider
+{
+    private $model;
+
+    public function __construct($userModel)
+    {
+        $this->model = $userModel;
+    }
+
+    public function retrieveById($identifier)
+    {
+        return $this->model->find($identifier)->first();
+    }
+
+    public function retrieveByCredentials(array $credentials) {
+        logger(implode(' --- ', $credentials));
+        $check = array_key_exists('username', $credentials) ? 'username' : 'email';
+        $check = $credentials[$check];
+
+        return $this->model->query()
+            ->where('username', $check)
+            ->orWhere('email', $check)
+            ->first();
+    }
+
+    public function validateCredentials(Authenticatable $user, array $credentials) {
+        ee()->load->library('auth');
+
+        $hashed = ee()->auth->hash_password($credentials['password']);
+        $sess = false;
+
+        if(array_key_exists('username', $credentials))
+        {
+            $sess = ee()->auth->authenticate_username($credentials['username'], $credentials['password']);
+        }
+
+        if(array_key_exists('email', $credentials))
+        {
+            $sess = ee()->auth->authenticate_email($credentials['email'], $credentials['password']);
+        }
+
+        if(!$sess)
+        {
+            return false;
+        }
+
+        $sess->start_session();
+
+        return $sess;
+    }
+
+    public function retrieveByToken($identifier, $token)
+    {
+        return null;
+    }
+
+    public function updateRememberToken(Authenticatable $user, $token) {
+        return null;
+    }
+}
